6e8a9dc01348f9c4bf26d74a2fee3082ddd7e0f3
Megan Adams <meganoliviawendyadams@gmail.com>
2019-08-28 03:57:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
     const h2 = document.createElement('h2')
-   h2.innerHTML = book.name
-    book.appendChild(h2)
+
+    h2.innerHTML = `<h2>${book.name}</h2>`
+    main.appendChild(h2)
   })
 }
 
